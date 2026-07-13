16bfb3db22ffc478405effde2edf9bb75dda7e77
amelmakharomi-glitch <amel.makharomi.com>
2026-07-13 14:20:16 +0700

    Add category controller

---
+++ src/main/java/com/umkmconnect/umkm_connect/controller/CategoryController.java
@@ -0,0 +1,35 @@
+package com.umkmconnect.umkm_connect.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.umkmconnect.umkm_connect.entity.Category;
+import com.umkmconnect.umkm_connect.service.CategoryService;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+        return categoryService.getCategoryById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
