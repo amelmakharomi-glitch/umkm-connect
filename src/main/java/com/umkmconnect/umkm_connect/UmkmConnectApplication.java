fe73f3ea7d1ff6be6ce579a3d76a3f73979c30d7
amelmakharomi-glitch <amel.makharomi.com>
2026-07-13 09:25:59 +0700

    Initial Spring Boot setup

---
+++ src/main/java/com/umkmconnect/umkm_connect/UmkmConnectApplication.java
@@ -0,0 +1,13 @@
+package com.umkmconnect.umkm_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmkmConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmkmConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umkmconnect/umkm_connect/UmkmConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umkmconnect.umkm_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmkmConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
