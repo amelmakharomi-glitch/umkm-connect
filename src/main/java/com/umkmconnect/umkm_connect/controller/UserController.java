3638fce6e074fb6bce43cd76e80a6d56b7e51ff4
amelmakharomi-glitch <amel.makharomi.com>
2026-07-13 11:23:29 +0700

    Add user controller

---
+++ src/main/java/com/umkmconnect/umkm_connect/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.umkmconnect.umkm_connect.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.umkmconnect.umkm_connect.entity.User;
+import com.umkmconnect.umkm_connect.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
